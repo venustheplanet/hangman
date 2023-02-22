f452d7d49306fd0ab18c5b072bf1956f72fb42d6
Venus <venus.cheung@outlook.com>
2023-02-22 13:56:06 +0000

    task 2-5

--- milestone_2.py
+++ milestone_2.py
@@ -1,7 +1,16 @@
 import random
 
+# Task 1
 word_list = ['banana', 'apple', 'kiwi', 'pear', 'orange']
 print(word_list)
 
+# Task 2
 word = random.choice(word_list)
 print(word)
+
+# Task 3
+guess = input('Please enter a single letter: ')
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
